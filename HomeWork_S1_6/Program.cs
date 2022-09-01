f9697973fac56719233ceee5089f12be9a900a68
Natalia <kotovana_92@mail.ru>
2022-09-01 16:44:23 +0800

    Решение задачи 6 определение четности числа

---
+++ HomeWork_S1_6/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+
+if ((a % 2) == 0) {
+    System.Console.WriteLine("число a является четным");
+}
+else{
+    System.Console.WriteLine("число a является нечетным"); 
+}
