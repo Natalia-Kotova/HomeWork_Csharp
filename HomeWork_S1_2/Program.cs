920d40ba786ff2e1dea7f4e029086ede4cf422fa
Natalia <kotovana_92@mail.ru>
2022-09-01 16:56:26 +0800

    Решение задачи 2 из двух чисел определяет максимум и минимум

---
+++ HomeWork_S1_2/Program.cs
@@ -0,0 +1,18 @@
+﻿System.Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+
+System.Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine ());
+
+if (a > b) {
+    System.Console.Write("max =");
+    System.Console.WriteLine(a);
+    System.Console.Write("min =");
+    System.Console.WriteLine(b);
+}
+else{
+    System.Console.Write("max =");
+    System.Console.WriteLine(b);
+    System.Console.Write("min =");
+    System.Console.WriteLine(a);
+}
